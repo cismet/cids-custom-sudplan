a34828ef8898a8b90a70aa9b1edd3f14e22f6d26
Daniel Meiers <daniel.meiers@cismet.de>
2011-11-15 15:10:31 +0000

    [issue:2309] testing timeseries retrival from SOS

---
+++ src/test/java/de/cismet/cids/custom/sudplan/SOSTest.java
@@ -0,0 +1,232 @@
+/***************************************************
+ *
+ * cismet GmbH, Saarbruecken, Germany
+ *
+ *              ... and it just works.
+ *
+ ****************************************************/
+package de.cismet.cids.custom.sudplan;
+
+import at.ac.ait.enviro.sudplan.sosclient.SOSClientHandler;
+import at.ac.ait.enviro.sudplan.util.EnvelopeQueryParameter;
+import at.ac.ait.enviro.tsapi.handler.DataHandler.Access;
+import at.ac.ait.enviro.tsapi.handler.Datapoint;
+import at.ac.ait.enviro.tsapi.timeseries.TimeInterval;
+import at.ac.ait.enviro.tsapi.timeseries.TimeSeries;
+import at.ac.ait.enviro.tsapi.timeseries.TimeStamp;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
+import java.io.File;
+import java.io.FileWriter;
+
+import junit.framework.Assert;
+
+import org.apache.log4j.Logger;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import org.openide.util.Exceptions;
+
+import java.io.IOException;
+
+import java.net.MalformedURLException;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   dmeiers
+ * @version  $Revision$, $Date$
+ */
+@Ignore
+public class SOSTest {
+
+    //~ Static fields/initializers ---------------------------------------------
+    private static final transient Logger LOG = Logger.getLogger(SOSTest.class);
+    private static Set<Datapoint> dps;
+    private static FileWriter w;
+    //~ Instance fields --------------------------------------------------------
+
+    //~ Constructors -----------------------------------------------------------
+    /**
+     * Creates a new SOSTest object.
+     */
+    public SOSTest() {
+    }
+
+    //~ Methods ----------------------------------------------------------------
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        File resultsFile = new File(System.getProperty("user.home") + System.getProperty("file.separator") + "SOSTestResults.txt");
+        w = new FileWriter(resultsFile);
+        final SOSClientHandler handler = new SOSClientHandler();
+
+        try {
+            handler.getConnector().connect("http://enviro3.ait.ac.at:8080");
+            handler.open();
+        } catch (MalformedURLException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+
+        dps = handler.getDatapoints(null, Access.DONT_CARE);
+
+
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        w.close();
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * DOCUMENT ME!
+     * @throws IOException 
+     */
+    @Test
+    public void retriveTSForSpecificPoint() throws IOException {
+        final Iterator<Datapoint> it = dps.iterator();
+
+        while (it.hasNext()) {
+            final Datapoint dp = it.next();
+            LOG.info("starting timeseries retrieval for Offering: " + dp.toString());
+            w.write("starting timeseries retrieval for Offering: " + dp.toString() + "n");
+            LOG.info("Filter: " + dp.getFilter().toString());
+            w.write("Filter: " + dp.getFilter().toString() + "\n");
+            final Map<String, Object> props = dp.getProperties();
+            final Envelope env = (Envelope) props.get("ts:geometry");
+
+            final double maxX = env.getMaxX();
+            final double maxY = env.getMaxY();
+            final double minX = env.getMinX();
+            final double minY = env.getMinY();
+            for (int i = 0; i < 21; i++) {
+                /*
+                 * calculate a random point
+                 */
+                final Random r = new Random();
+                final double x = (r.nextDouble() * (maxX - minX + 1)) + minX;
+                final double y = (r.nextDouble() * (maxY - minY + 1)) + minY;
+
+                Assert.assertTrue(minX <= x);
+                Assert.assertTrue(x <= maxX);
+                Assert.assertTrue(minY <= y);
+                Assert.assertTrue(y <= maxY);
+                /*
+                 * calculate a random time interval
+                 */
+                final Date minDate = (Date) props.get("ts:available_data_min");
+                final Date maxDate = (Date) props.get("ts:available_data_max");
+                final long start = (r.nextLong() * (maxDate.getTime() - minDate.getTime() + 1)) + minDate.getTime();
+                final long end = (r.nextLong() * (maxDate.getTime() - minDate.getTime() + 1)) + minDate.getTime();
+
+                Assert.assertTrue(minDate.getTime() <= start);
+                Assert.assertTrue(start <= maxDate.getTime());
+                Assert.assertTrue(minDate.getTime() <= end);
+                Assert.assertTrue(end <= maxDate.getTime());
+                final GregorianCalendar startDate = new GregorianCalendar();
+                startDate.setTimeInMillis(start);
+                final GregorianCalendar endDate = new GregorianCalendar();
+                endDate.setTimeInMillis(end);
+
+
+                final TimeInterval interval = new TimeInterval(
+                        TimeInterval.Openness.OPEN,
+                        new TimeStamp(startDate.getTime()),
+                        new TimeStamp(endDate.getTime()),
+                        TimeInterval.Openness.OPEN);
+
+
+                final EnvelopeQueryParameter point = new EnvelopeQueryParameter();
+                point.setEnvelope(new Envelope(new Coordinate(x, y)));
+                long startTime;
+                long endTime;
+                TimeSeries ts = null;
+                try {
+                    /*
+                     * TimeSeriesRetrieval for specific point
+                     */
+                    LOG.info("TimeSeriesRetrieval " + i + ", for Point: " + x + " / " + y);
+                    w.write("\t TimeSeriesRetrieval " + i + ", for Point: " + x + " / " + y);
+                    startTime = System.currentTimeMillis();
+                    ts = dp.getTimeSeries(TimeInterval.ALL_INTERVAL, point);
+                    endTime = System.currentTimeMillis();
+
+//                    Assert.assertNotNull(ts);
+                    if (ts != null) {
+                        LOG.info(" - retrieval succesfull ");
+                        w.write(" - retrieval succesfull \n");
+                        LOG.info("Time: " + ((endTime - startTime) / 1000) + " secs");
+                        w.write("\t Time: " + ((endTime - startTime) / 1000) + " secs \n");
+                    } else {
+                        LOG.error(" - retrieval failed");
+                        w.write(" - retrieval failed \n");
+                    }
+                    ts = null;
+                    /*
+                     * TimeSeriesRetrieval for specific interval and specific point
+                     */
+
+                    LOG.info("TimeSeriesRetrieval" + i + ", for Interval: " + startDate.getTime() + " / " + endDate.getTime());
+                    w.write(" \t TimeSeriesRetrieval" + i + ", for Interval: " + startDate.getTime() + " / " + endDate.getTime());
+                    startTime = System.currentTimeMillis();
+                    ts = dp.getTimeSeries(interval, point);
+                    endTime = System.currentTimeMillis();
+
+//                    Assert.assertNotNull(ts);
+                    if (ts != null) {
+                        LOG.info(" - retrieval succesfull ");
+                        w.write(" - retrieval succesfull \n");
+                        LOG.info("Time: " + ((endTime - startTime) / 1000) + " secs");
+                        w.write("\t Time: " + ((endTime - startTime) / 1000) + " secs \n");
+                    } else {
+                        LOG.error(" - retrieval failed");
+                        w.write(" - retrieval failed \n");
+                    }
+                } catch (Exception e) {
+                    LOG.error(" - retrieval failed", e);
+                    w.write(" - retrieval failed \n");
+                    continue;
+                }
+
+            }
+        }
+    }
+}
