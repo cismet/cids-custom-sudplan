5c2b836c0de6d3b1b1f489169ad111c55a8b267d
Martin Scholl <martin.scholl@cismet.de>
2012-08-27 17:47:40 +0200

    [issue #19] initial format hint interface

---
+++ src/main/java/de/cismet/cids/custom/sudplan/converter/FormatHint.java
@@ -0,0 +1,64 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.sudplan.converter;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   martin.scholl@cismet.de
+ * @version  $Revision$, $Date$
+ */
+public interface FormatHint {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * The name of the format, usually a machine readable format identifier.
+     *
+     * @return  the name of the format
+     */
+    String getFormatName();
+
+    /**
+     * Shall return a human readable display name for the format.
+     *
+     * @return  a human readable display name for the format
+     */
+    String getFormatDisplayName();
+
+    /**
+     * Shall return the format name as <code>HTML String</code> or <code>null</code> if there is no <code>HTML</code>
+     * formatted name <code>String</code>.
+     *
+     * @return  the <code>HTML</code> display name or <code>null</code> if there is none available
+     */
+    String getFormatHtmlName();
+
+    /**
+     * Shall return a human readable description of the supported format.
+     *
+     * @return  a human readable description of the supported format
+     */
+    String getFormatDescription();
+
+    /**
+     * Shall return a human readable <code>HTML String</code> or <code>null</code> if there is no <code>HTML</code>
+     * formatted description <code>String</code>.
+     *
+     * @return  a human readable <code>HTML</code> description or <code>null</code> if there is none available
+     */
+    String getFormatHtmlDescription();
+
+    /**
+     * Shall return a depiction of the format so that the user may see on a concrete example what the supported format
+     * looks like.
+     *
+     * @return  a format example or <code>null</code> if no example is available
+     */
+    Object getFormatExample();
+}
